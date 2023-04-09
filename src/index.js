115a40b331aca7b773896495b02de8b62b1a7f85
Dimitri Glazkov <dimitri@glazkov.com>
2023-04-08 17:24:28 -0700

    Employ duckdb.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import hnswlib from "hnswlib-node";
 import fs from "fs";
+import duckdb from "duckdb";
+import { table } from "console";
 
 const DIMENSIONS = 1536;
 
@@ -18,7 +20,7 @@ const loadLibrary = () => {
   return library;
 };
 
-const createIndex = async () => {
+const createDatabaseAndIndex = async () => {
   const library = loadLibrary();
 
   const hnsw = new hnswlib.HierarchicalNSW("l2", DIMENSIONS);
@@ -28,22 +30,55 @@ const createIndex = async () => {
     hnsw.addPoint(bit.embedding, i);
   });
 
+  const db = new duckdb.Database("./store.db");
+  const conn = db.connect();
+
+  conn.exec("DROP TABLE IF EXISTS library");
+
+  conn.exec(
+    "CREATE TABLE library (id INTEGER, text VARCHAR, url VARCHAR, token_count INTEGER)"
+  );
+
+  const insertion = conn.prepare("INSERT INTO library VALUES (?, ?, ?, ?)");
+  library.bits.forEach((bit, i) =>
+    insertion.run(i, bit.text, bit.info.url, bit.token_count)
+  );
+  await new Promise((resolve, reject) => {
+    insertion.finalize(resolve);
+  });
+
   await hnsw.writeIndex("./index.bin");
 };
 
-const useIndex = () => {
+const useIndex = async () => {
   const hnsw = new hnswlib.HierarchicalNSW("l2", DIMENSIONS);
-  hnsw.readIndexSync("./index.bin");
+  await hnsw.readIndex("./index.bin");
 
   const query = new Array(DIMENSIONS).fill(0).map(() => Math.random());
   const neighbors = 3;
   const results = hnsw.searchKnn(query, neighbors);
-  console.log(results);
+
+  console.log("Vector search results", results);
+
+  const db = new duckdb.Database("./store.db");
+  const conn = db.connect();
+
+  conn.all("SELECT count(*) FROM library", (err, rows) => {
+    console.log("Library size", err, rows);
+  });
+
+  conn.all(
+    "SELECT * FROM library WHERE id IN (?, ?, ?)",
+    ...results.neighbors,
+    (err, rows) => {
+      console.log("Text search results", err, rows);
+    }
+  );
 };
 
 const main = async () => {
-  await createIndex();
-  useIndex();
+  await createDatabaseAndIndex();
+  await useIndex();
 };
 
 main();
